36b2b6d60b4ab4850454f3696208606618f97ab3
정지성 <jisung@mdom17205-1.postech.ac.kr>
2020-11-27 08:15:38 +0900

    elif

--- neurocard/datasets.py
+++ neurocard/datasets.py
@@ -177,21 +177,21 @@ class TPC_DS(object) :
                 cols = TPC_DS._CONTENT_COLS[table_name]
                 if table_name == 'item.csv':
                     cols.append('i_item_sk')
-                else if  table_name == 'sales_store.csv' : 
+                elif  table_name == 'sales_store.csv' : 
                     cols.append('ss_item_sk')
-                else if  table_name == 'store_returns.csv' : 
+                elif  table_name == 'store_returns.csv' : 
                     cols.append('sr_item_sk')
-                else if  table_name == 'catalog_sales.csv' : 
+                elif  table_name == 'catalog_sales.csv' : 
                     cols.append('cs_item_sk') 
-                else if  table_name == 'catalog_returns.csv' : 
+                elif  table_name == 'catalog_returns.csv' : 
                     cols.append('cr_item_sk') 
-                else if  table_name == 'web_sales.csv' : 
+                elif  table_name == 'web_sales.csv' : 
                     cols.append('ws_item_sk') 
-                else if  table_name == 'web_returns.csv' : 
+                elif  table_name == 'web_returns.csv' : 
                     cols.append('wr_item_sk') 
-                else if  table_name == 'inventory.csv' : 
+                elif  table_name == 'inventory.csv' : 
                     cols.append('inv_item_sk') 
-                else if  table_name == 'promotion.csv' : 
+                elif  table_name == 'promotion.csv' : 
                     cols.append('p_item_sk')
 
         return TPC_DS._CONTENT_COLS
